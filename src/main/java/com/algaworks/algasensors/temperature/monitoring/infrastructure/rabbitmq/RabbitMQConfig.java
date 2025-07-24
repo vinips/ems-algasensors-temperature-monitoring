6b465d1725c28995d854ce234690e5aeb43e1275
Vini Silveira <viniciusps_ph@hotmail.com>
2025-07-24 15:50:08 -0300

    Finalizando modulo 13.4
    - Criando a Dead Letter Queue, implementando o Dead Letter Pattern, para que mensagens descartadas apos x tentativas de leitura seajm enviadas para essa fila em vez de serem totalmente descartadas.

--- src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQConfig.java
+++ src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQConfig.java
@@ -8,12 +8,20 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class RabbitMQConfig {
 
     public static final String FANOUT_EXCHANGE_PROCESS_TEMPERATURE = "temperature-processing.temperature.received.v1.e";
-    public static final String QUEUE_PROCESS_TEMPERATURE = "temperature-monitoring.process-temperature.v1.q";
     public static final String QUEUE_SENSOR_ALERT = "temperature-monitoring.sensor-alert.v1.q";
+
+    private static final String PROCESS_TEMPERATURE = "temperature-monitoring.process-temperature.v1";
+    public static final String QUEUE_PROCESS_TEMPERATURE = PROCESS_TEMPERATURE + ".q";
+    public static final String DEAD_LETTER_QUEUE_PROCESS_TEMPERATURE = PROCESS_TEMPERATURE + "dlq";
+
+
 
     //Esse Bean é necessario para converter um Objeto complexo (TemperatureLogData por exemplo)
     //Sem esse bean ele nao consegue converter e da erro na hora de enviar para fila.
@@ -29,8 +37,22 @@ public class RabbitMQConfig {
 
     @Bean
     public Queue queueProcessTemperature() {
+        //configuracao necessaria para utilizar o Dead Letter Pattern.
+        //Que cria uma queue para receber as mensagens descartadas devido ao Retry Pattern.
+        Map<String, Object> args = new HashMap<>();
+        args.put("x-dead-letter-exchange", "");
+        args.put("x-dead-letter-routing-key", DEAD_LETTER_QUEUE_PROCESS_TEMPERATURE);
+
         return QueueBuilder
                 .durable(QUEUE_PROCESS_TEMPERATURE)
+                .withArguments(args)
+                .build();
+    }
+
+    @Bean
+    public Queue deadLetterQueueProcessTemperature() {
+        return QueueBuilder
+                .durable(DEAD_LETTER_QUEUE_PROCESS_TEMPERATURE)
                 .build();
     }
 
